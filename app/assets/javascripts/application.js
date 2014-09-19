6bdd7ad3a7f2bd3f9dd8996f89c54cf8b7458f6a
Mapik <pikoszm.m@gmail.com>
2014-09-19 23:49:57 +0200

    comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
+//= require turbolinks
+//= require_tree .
 
+//= require init
+
+$(function(){ $(document).foundation(); });
